a7ca4ada885555934a48669305ddd85c39134aff
Abhicharan08 <73337530+Abhicharan08@users.noreply.github.com>
2025-07-20 15:36:01 +0530

    chnages

---
+++ .vite/deps/react-bootstrap_Accordion.js
@@ -0,0 +1,10 @@
+"use client";
+import {
+  Accordion_default
+} from "./chunk-IXH7P57K.js";
+import "./chunk-N5P5IXHH.js";
+import "./chunk-54FEGWNZ.js";
+import "./chunk-5WRI5ZAA.js";
+export {
+  Accordion_default as default
+};
